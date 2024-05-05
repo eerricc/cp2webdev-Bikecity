522ac75ae0801c537d64ec4cdcc6dbfda9f7a467
Gabriel Luni Nakashima <46712524+GabrielNakashima@users.noreply.github.com>
2024-05-05 18:37:32 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+//alert("Seja bem vindo")
+
+let imagens=["./pimba/img/Bike1.jpg", "./pimba/img/Bike2.jpg", "./pimba/img/Bike3.jpg", "./pimba/img/Bike4.jpg"];
+let index = 0;
+let time =3000;
+
+function slideShow(){
+
+document.getElementById("imgbanner").src=imagens[index];
+index++;
+
+    if(index == imagens.length){
+        index=0;
+    }
+    setTimeout("slideShow()", time);
+}
+slideShow();
+
+
+var cores = ["#ff0000", "#00ff00", "#0000ff"];
+var indiceCorAtual = 0;   
+    
+function mudarCor() {
+    
+    var corpo = document.body;
+    corpo.style.backgroundColor = cores[indiceCorAtual];
+    indiceCorAtual = (indiceCorAtual + 1) % cores.length;
+}
+
+
+
+
+
+
+function validarFormulario() {
+    // Obter os valores do nome e senha inseridos no formulário
+    var nome = document.getElementById("nome").value;
+    var senha = document.getElementById("senha").value;
+
+    if (nome === "usuario" && senha === "1234") {
+        alert("Redirecionando")
+        window.location.href = "quiz.html";
+        return false; // Impede o envio do formulário após o redirecionamento
+    } else {
+        alert("Nome de usuário ou senha incorretos!");
+        return true; // Impedir o envio do formulário
+    }
+}
